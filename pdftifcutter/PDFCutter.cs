128aa0de13c42d265ce6e5d2c2c47e6c0d064c35
kenjiuno <h8mastre@gmail.com>
2015-12-11 14:18:22 +0900

    0.1

---
+++ pdftifcutter/PDFCutter.cs
@@ -0,0 +1,52 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pdftifcutter {
+    class PDFCutter : Cutter {
+        PdfReader reader;
+
+        public PDFCutter(string fpin) {
+            reader = new PdfReader(fpin);
+            reader.ConsolidateNamedDestinations();
+        }
+
+        public override IWr New(string fpout) {
+            return new PDFW(fpout, reader);
+        }
+    }
+
+    class PDFW : IWr {
+        PdfReader reader;
+        Document document;
+        PdfCopy copy;
+        FileStream fs;
+
+        public PDFW(String fpout, PdfReader reader) {
+            this.reader = reader;
+            this.fs = File.Create(fpout);
+        }
+
+        public void Add(int pi) {
+            if (document == null) {
+                document = new Document(reader.GetPageSizeWithRotation(pi));
+                copy = new PdfCopy(document, fs);
+                document.Open();
+            }
+
+            PdfImportedPage page = copy.GetImportedPage(reader, pi);
+            copy.AddPage(page);
+        }
+
+        public void Dispose() {
+            copy.Close();
+            document.Close();
+            fs.Close();
+        }
+    }
+}
--- pdftifcutter/Program.cs
+++ pdftifcutter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,60 @@ using System.Threading.Tasks;
 namespace pdftifcutter {
     class Program {
         static void Main(string[] args) {
+            if (args.Length < 3) {
+                helpYa(); Environment.ExitCode = 1;
+                return;
+            }
+            try {
+                String fpin = args[0];
+                Cutter cutter = Cutter.Get(fpin);
+                int x = 1, cx = args.Length;
+                while (x < cx) {
+                    if (args[x] != "(") throw new ArgumentException();
+                    x++;
+                    String fpout = args[x];
+                    using (var wr = cutter.New(fpout)) {
+                        x++;
+                        while (args[x] != ")") {
+                            int pi = (int.Parse(args[x]));
+                            x++;
+                            wr.Add(pi);
+                        }
+                        x++;
+                    }
+                }
+            }
+            catch (Exception err) {
+                Console.Error.WriteLine("" + err);
+                Environment.ExitCode = 2;
+            }
         }
+
+        private static void helpYa() {
+            Console.Error.WriteLine("pdftifcutter in.pdf ( out-even.pdf 2 4 6 ) ( out-odd.pdf 1 3 5 )");
+            Console.Error.WriteLine("pdftifcutter in.tif ( out-even.tif 2 4 6 ) ( out-odd.tif 1 3 5 )");
+        }
+    }
+
+    abstract class Cutter {
+        public static Cutter Get(string fpin) {
+            if ("|.tif|.tiff|".IndexOf(Path.GetExtension(fpin), StringComparison.InvariantCultureIgnoreCase) >= 0) {
+                return new TIFCutter(fpin);
+            }
+            if ("|.pdf|".IndexOf(Path.GetExtension(fpin), StringComparison.InvariantCultureIgnoreCase) >= 0) {
+                return new PDFCutter(fpin);
+            }
+            throw new NotSupportedException(Path.GetExtension(fpin));
+        }
+
+        public abstract IWr New(string fpout);
+    }
+
+    interface IWr : IDisposable {
+        /// <summary>
+        /// Add the page
+        /// </summary>
+        /// <param name="pi">1 based</param>
+        void Add(int pi);
     }
 }
