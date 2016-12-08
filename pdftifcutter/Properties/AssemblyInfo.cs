223ab5ad76e7e5f3fe01c2e6c8eb05fe743540d9
kenjiuno <ku@digitaldolphins.jp>
2016-12-08 12:03:09 +0900

    0.3

--- pdftifcutter/Properties/AssemblyInfo.cs
+++ pdftifcutter/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("pdftifcutter")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2015,2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly:     AssemblyVersion("0.2")]//here
-[assembly: AssemblyFileVersion("0.2")]//here
+[assembly:     AssemblyVersion("0.3")]//here
+[assembly: AssemblyFileVersion("0.3")]//here
