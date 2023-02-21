2027637c0c5958368a5949fb7810ac19786ec0b0
kenjiuno <ku@digitaldolphins.jp>
2023-02-22 00:28:49 +0900

    0.4

--- pdftifcutter/Properties/AssemblyInfo.cs
+++ pdftifcutter/Properties/AssemblyInfo.cs
@@ -2,6 +2,10 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+[assembly: AssemblyFileVersion("0.4")]
+
+[assembly: InternalsVisibleTo("pdftifcutter.tests")]
+
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
@@ -10,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("pdftifcutter")]
-[assembly: AssemblyCopyright("Copyright ©  2015,2016")]
+[assembly: AssemblyCopyright("Copyright ©  2015,2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +36,4 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly:     AssemblyVersion("0.3")]//here
-[assembly: AssemblyFileVersion("0.3")]//here
+[assembly: AssemblyVersion("1.0.0.0")]
